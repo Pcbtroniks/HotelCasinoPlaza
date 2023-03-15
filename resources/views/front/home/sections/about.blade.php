c5af8b551a6977d3e76582d6b4f91da7e92b3e71
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2023-03-15 16:27:27 -0600

    Add\ Responsive Home PAge

--- resources/views/front/home/sections/about.blade.php
+++ resources/views/front/home/sections/about.blade.php
@@ -1,22 +1,22 @@
 <section>
 
     <div class="px-8 py-[5.5rem] flex flex-wrap max-w-7xl mx-auto">
-        <div class="w-1/2">
+        <div class="lg:w-1/2">
             <h2 class="text-3xl font-bold">
                 Hotel Casino Plaza
             </h2>
-            <span class="border-b-4 border-main block w-72 my-2"></span>
+            <span class="border-b-4 border-main block lg:w-72 my-2"></span>
             <p class="text-xl">
                 En la ciudad de Guadalajara
             </p>
             <br>
-            <p class="pr-8 text-justify text-dark-text">
+            <p class="pr-8 text-justify text-dark-text mb-8 lg:mb-0">
                 El Hotel Casino Plaza en Guadalajara es un hotel 4 estrellas con una ubicación céntrica. Con 65 habitaciones, el hotel ofrece servicios de alta calidad, incluyendo TV por cable, internet, aire acondicionado, caja de seguridad, estacionamiento gratuito y servicio a cuarto. Además, el hotel tiene suites con jacuzzi, columna de hidromasaje, frigo bar y DVD. Los huéspedes pueden reservar directamente en línea para obtener la mejor tarifa disponible.
             </p>
         </div>
         <!-- -->
-        <div class="w-1/2 overflow-hidden card-shadow">
-            <img class="w-full bg-cover object-cover scale-150" src="{{ asset('assets/img/about.jpg') }}" alt="About us Image">
+        <div class="lg:w-1/2 overflow-hidden card-shadow">
+            <img class="w-full bg-cover object-cover scale-150 h-full" src="{{ asset('assets/img/about.jpg') }}" alt="About us Image">
         </div>
     </div>
 
--- resources/views/front/home/sections/rooms.blade.php
+++ resources/views/front/home/sections/rooms.blade.php
@@ -11,9 +11,9 @@
                 <span class="border-b-4 border-main block w-80 my-2"></span>
             </div>
             
-            <div class="columns-2">
+            <div class="md:columns-xs lg:columns-sm xl:columns-xl">
                 <!--Room Card-->
-                <div class="shadow-card card-shadow max-w-[38rem] mx-auto w-full">
+                <div class="shadow-card card-shadow max-w-[38rem] mx-auto w-full mb-8">
                     
                     <div class="w-full h-[230px] overflow-hidden">
                         <img class="w-full h-full object-cover hover:scale-110 transition-transform duration-300" src="{{asset('assets/img/rooms/ejecutiva.jpg')}}" alt="Habitación ejecutiva">
@@ -31,7 +31,7 @@
                             Esta habitación cuenta con una mesa de trabajo, equipada con una cama King Size perfecta para una buena noche de sueño.
                         </p>
                         
-                        <ul class="flex mt-3 gap-4">
+                        <ul class="flex my-3 gap-4">
                             <li><img src="{{ asset('assets/icons/tv.png') }}" alt="service"></li>
                             <li><img src="{{ asset('assets/icons/wifi.png') }}" alt="service"></li>
                             <li><img src="{{ asset('assets/icons/air.png') }}" alt="service"></li>
@@ -50,42 +50,81 @@
                     
                 </div>
                 <!--Room Card-->
-                <div class="shadow-card card-shadow max-w-[38rem] mx-auto w-full">
+                <div class="shadow-card card-shadow max-w-[38rem] mx-auto w-full mb-8">
                     
                     <div class="w-full h-[230px] overflow-hidden">
                         <img class="w-full h-full object-cover hover:scale-110 transition-transform duration-300" src="{{asset('assets/img/rooms/ejecutiva.jpg')}}" alt="Habitación ejecutiva">
                     </div>
+                    
+                    <div class="p-6 pb-4 ">
+                        
+                        <h3 class="font-bold mb-3 text-xl">
+                            Habitación Sencilla
+                        </h3>
+                        
+                        <p class="text-sm leading-6">
+                            Habitación Sencilla, especialmente diseñada para tu comodidad y conveniencia, recomendada para 2 personas.
+                            <br>
+                            Esta habitación cuenta con una mesa de trabajo, equipada con una cama King Size perfecta para una buena noche de sueño.
+                        </p>
+                        
+                        <ul class="flex my-3 gap-4">
+                            <li><img src="{{ asset('assets/icons/tv.png') }}" alt="service"></li>
+                            <li><img src="{{ asset('assets/icons/wifi.png') }}" alt="service"></li>
+                            <li><img src="{{ asset('assets/icons/air.png') }}" alt="service"></li>
+                            <li><img src="{{ asset('assets/icons/park.png') }}" alt="service"></li>
+                            <li><img src="{{ asset('assets/icons/pool.png') }}" alt="service"></li>
+                        </ul>
+                        
+                        <div>
+                            <div class="space-y md:h-5"></div>
+                            <a href="{{route('contact')}}">
+                                <button class="h-12 w-36 bg-main text-white text-sm font-bold">Reservar Ahora</button>
+                            </a>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+                <!--Room Card-->
+                <div class="shadow-card card-shadow max-w-[38rem] mx-auto w-full mb-8 break-before-column">
+                    
+                    <div class="w-full h-[230px] overflow-hidden">
+                        <img class="w-full h-full object-cover hover:scale-110 transition-transform duration-300" src="{{asset('assets/img/rooms/ejecutiva.jpg')}}" alt="Habitación ejecutiva">
+                    </div>
+                    
+                    <div class="p-6 pb-4 ">
+                        
+                        <h3 class="font-bold mb-3 text-xl">
+                            Habitación Sencilla
+                        </h3>
+                        
+                        <p class="text-sm leading-6">
+                            Habitación Sencilla, especialmente diseñada para tu comodidad y conveniencia, recomendada para 2 personas.
+                            <br>
+                            Esta habitación cuenta con una mesa de trabajo, equipada con una cama King Size perfecta para una buena noche de sueño.
+                        </p>
+                        
+                        <ul class="flex my-3 gap-4">
+                            <li><img src="{{ asset('assets/icons/tv.png') }}" alt="service"></li>
+                            <li><img src="{{ asset('assets/icons/wifi.png') }}" alt="service"></li>
+                            <li><img src="{{ asset('assets/icons/air.png') }}" alt="service"></li>
+                            <li><img src="{{ asset('assets/icons/park.png') }}" alt="service"></li>
+                            <li><img src="{{ asset('assets/icons/pool.png') }}" alt="service"></li>
+                        </ul>
+                        
+                        <div>
+                            <div class="space-y md:h-5"></div>
+                            <a href="{{route('contact')}}">
+                                <button class="h-12 w-36 bg-main text-white text-sm font-bold">Reservar Ahora</button>
+                            </a>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
 
-                    <div class="p-6 pb-4 ">
-                        
-                        <h3 class="font-bold mb-3 text-xl">
-                            Habitación Sencilla
-                        </h3>
-                        
-                        <p class="text-sm leading-6">
-                            Habitación Sencilla, especialmente diseñada para tu comodidad y conveniencia, recomendada para 2 personas.
-                            <br>
-                            Esta habitación cuenta con una mesa de trabajo, equipada con una cama King Size perfecta para una buena noche de sueño.
-                        </p>
-                        
-                        <ul class="flex mt-3 gap-4">
-                            <li><img src="{{ asset('assets/icons/tv.png') }}" alt="service"></li>
-                            <li><img src="{{ asset('assets/icons/wifi.png') }}" alt="service"></li>
-                            <li><img src="{{ asset('assets/icons/air.png') }}" alt="service"></li>
-                            <li><img src="{{ asset('assets/icons/park.png') }}" alt="service"></li>
-                            <li><img src="{{ asset('assets/icons/pool.png') }}" alt="service"></li>
-                        </ul>
-                        
-                        <div>
-                            <div class="space-y md:h-5"></div>
-                            <a href="{{route('contact')}}">
-                                <button class="h-12 w-36 bg-main text-white text-sm font-bold">Reservar Ahora</button>
-                            </a>
-                        </div>
-                        
-                    </div>
-                    
-                </div>
+
         </div>
         
     </div>
