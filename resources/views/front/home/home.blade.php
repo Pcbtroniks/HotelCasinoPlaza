b10d21e8a11ffc60ed76c6475ec1478d46c61eba
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2023-03-23 11:12:28 -0600

    Add\ Hero carousel

--- resources/views/front/home/home.blade.php
+++ resources/views/front/home/home.blade.php
@@ -1,5 +1,34 @@
 @extends('layout.base')
 
+@section('title', 'Home')
+
+@section('css')
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+@endsection
+@section('js')
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <script>
+    const swiper3 = new Swiper("#hero-carousel", {
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        parallax: true,
+        loop: true,
+        spaceBetween: 30,
+        effect: "fade",
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+    </script>
+@endsection
+
 @section('content')
     <div>
         @include('front.home.sections.hero')
--- resources/views/front/home/sections/hero.blade.php
+++ resources/views/front/home/sections/hero.blade.php
@@ -1,21 +1,86 @@
-<section>
-    <div class="hero-bg bg-cover w-full h-screen relative">
+<section id="hero-carousel" class="swiper w-full h-screen mySwiper3">
+    <div class="swiper-wrapper hero-bg bg-cover w-full h-screen relative">
+        
+        <div class="swiper-slide">
 
-        <div id="hero-carousel" class="owl-carousel dark-overlay absolute -z-10 top-0 left-0 h-full w-full overflow-hidden">
-                <img class="w-full h-full bg-cover object-cover" src="{{ asset('/assets/img/hero/principal-1.jpg') }}" alt="Hero Background Image">
-        </div>
+            <div class="dark-overlay absolute -z-10 top-0 left-0 h-full w-full overflow-hidden">
+                    <img class="w-full h-full bg-cover object-cover" src="{{ asset('/assets/img/hero/principal-1.jpg') }}" alt="Hero Background Image">
+            </div>
 
-        <div class="text-white text-center flex flex-col justify-center gap-3 h-full z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <h1 class="text-5xl font-bold px-12">
-                Hotel Casino Plaza
-            </h1>
-            <h2 class="text-5xl font-light">
-                Confort y Descanso a tu alcance
-            </h2>
-            <span class="w-[21rem] lg:w-[41rem]  mx-auto border-b-4 border-main"></span>
-            <p class="text-xl px-10">
-                ESTAMOS SEGUROS DE PODER ALCANZAR SUS MÁS ALTAS EXPECTATIVAS, CONSIDERANDO QUE DÍA A DÍAMEJORAMOS LA CALIDAD EN NUESTROS SERVICIOS E INSTALACIONES.
-            </p>
-        </div>
+            <div class="text-white text-center flex flex-col justify-center gap-3 h-full z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <h1 class="text-5xl font-bold px-12">
+                    Hotel Casino Plaza
+                </h1>
+                <h2 class="text-5xl font-light" data-swiper-parallax-duration="400" data-swiper-parallax="-300">
+                    Confort y Descanso a tu alcance
+                </h2>
+                <span class="w-[21rem] lg:w-[41rem]  mx-auto border-b-4 border-main"></span>
+                <p class="text-xl px-10">
+                    ESTAMOS SEGUROS DE PODER ALCANZAR SUS MÁS ALTAS EXPECTATIVAS, CONSIDERANDO QUE DÍA A DÍAMEJORAMOS LA CALIDAD EN NUESTROS SERVICIOS E INSTALACIONES.
+                </p>
+            </div>
+
+        </div>    
+        <div class="swiper-slide">
+
+            <div class="dark-overlay absolute -z-10 top-0 left-0 h-full w-full overflow-hidden">
+                    <img class="w-full h-full bg-cover object-cover" src="{{ asset('assets/img/rooms/ejecutiva.jpg') }}" alt="Hero Background Image">
+            </div>
+
+            <div class="text-white text-center flex flex-col justify-center gap-3 h-full z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <h1 class="text-5xl font-bold px-12" >
+                    Hotel Casino Plaza
+                </h1>
+                <h2 class="text-5xl font-light" data-swiper-parallax-duration="400" data-swiper-parallax="-300">
+                    Perfecto para estancias largas
+                </h2>
+                <span class="w-[21rem] lg:w-[41rem]  mx-auto border-b-4 border-main"></span>
+                <p class="text-xl px-10">
+                    ESTAMOS SEGUROS DE PODER ALCANZAR SUS MÁS ALTAS EXPECTATIVAS, CONSIDERANDO QUE DÍA A DÍAMEJORAMOS LA CALIDAD EN NUESTROS SERVICIOS E INSTALACIONES.
+                </p>
+            </div>
+
+        </div>    
+        <div class="swiper-slide">
+
+            <div class="dark-overlay absolute -z-10 top-0 left-0 h-full w-full overflow-hidden">
+                    <img class="w-full h-full bg-cover object-cover" src="{{ asset('assets/img/about.jpg') }}" alt="Hero Background Image">
+            </div>
+
+            <div class="text-white text-center flex flex-col justify-center gap-3 h-full z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <h1 class="text-5xl font-bold px-12" >
+                    Hotel Casino Plaza
+                </h1>
+                <h2 class="text-5xl font-light" data-swiper-parallax-duration="400" data-swiper-parallax="-300">
+                    En el corazón de la perla tapatía
+                </h2>
+                <span class="w-[21rem] lg:w-[41rem]  mx-auto border-b-4 border-main"></span>
+                <p class="text-xl px-10">
+                    ESTAMOS SEGUROS DE PODER ALCANZAR SUS MÁS ALTAS EXPECTATIVAS, CONSIDERANDO QUE DÍA A DÍAMEJORAMOS LA CALIDAD EN NUESTROS SERVICIOS E INSTALACIONES.
+                </p>
+            </div>
+
+        </div>    
+        <div class="swiper-slide">
+
+            <div class="dark-overlay absolute -z-10 top-0 left-0 h-full w-full overflow-hidden">
+                    <img class="w-full h-full bg-cover object-cover" src="{{ asset('assets/img/installations/restarant.jpg') }}" alt="Hero Background Image">
+            </div>
+
+            <div class="text-white text-center flex flex-col justify-center gap-3 h-full z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <h1 class="text-5xl font-bold px-12" >
+                    Hotel Casino Plaza
+                </h1>
+                <h2 class="text-5xl font-light" data-swiper-parallax-duration="400" data-swiper-parallax="-300">
+                    La comodidad al alcance de una reserva.
+                </h2>
+                <span class="w-[21rem] lg:w-[41rem]  mx-auto border-b-4 border-main"></span>
+                <p class="text-xl px-10">
+                    ESTAMOS SEGUROS DE PODER ALCANZAR SUS MÁS ALTAS EXPECTATIVAS, CONSIDERANDO QUE DÍA A DÍAMEJORAMOS LA CALIDAD EN NUESTROS SERVICIOS E INSTALACIONES.
+                </p>
+            </div>
+
+        </div>    
+    
     </div>
 </section>
